47eb54a8296fbebd051aca2c35938e3c874b4b6c
Mapu456 <mpaula456@hotmail.com>
2022-02-24 14:39:25 -0500

    debuggong 4, point 1

--- checkbox.js
+++ checkbox.js
@@ -1,58 +1,52 @@
 //evento = cajita seleccionada
 // not exactly vanilla as there is one lodash function
-var allCheckboxes = document.querySelectorAll("input[type=checkbox]");
-console.log(allCheckboxes); // lista todas las cajitas o checkboxes listadas sin titulo
-var allProducts = Array.from(document.querySelectorAll(".products"));
-console.log("************************", allProducts); // lista la clase de los productos
-var checked = {};
+const allCheckboxes = document.querySelectorAll("input[type=checkbox]");
+//console.log(allCheckboxes); // lista todas las cajitas o checkboxes listadas sin titulo
+const allProducts = Array.from(document.querySelectorAll(".products"));
+//console.log("************************", allProducts); // lista la clase de los productos
+const checked = {
+  "categories":[],
+  "tags":[],
+  "availability":[]
+};
 
-//getChecked() is used to check whether a user has checked a checkbox or
-//radio button by using the element ID
-//proof
-getChecked("categories");
-getChecked("tags");
-getChecked("availability");
 
-//El array no se usa en absoluto pero da acceso al uso del prototipo foreach
-// y call es un prototipo que llama a otras funciones
+
 Array.prototype.forEach.call(allCheckboxes, function (el) {
-  console.log("this is a proof", el);
   el.addEventListener("change", toggleCheckbox); // el type del evento es cambiado a de "checkbox"->"change" (cambio de estado el checkbox)
-}); //y vaya a la funcion toggleCheckbox
+}); 
 
 function toggleCheckbox(e) {
-  console.log("this is a proof e ", e); //"e" es la clase de cada checkbox seleccionado
-  console.log("this is a proof e ************", e.target.name); //esta seleccionando los titulos de cada cajita seleccionada
-  getChecked(e.target.name); // se pasa el titulo de la cajita seleccionada y retorna el nombre del evento (frozen, drinks..)
-  setVisibility();
+  const productsFiltered = [...allProducts].filter(p => p?.className.includes(e.target.value));
+
+ getChecked(e.target.name);
+ // se pasa el titulo de la cajita seleccionada y retorna el nombre del evento (frozen, drinks..)
+  setVisibility(productsFiltered);
 }
 
 function getChecked(name) {
-  //al titulo se le asigna el nombre del evento que fue checkeado
   checked[name] = Array.from(
     document.querySelectorAll("input[name=" + name + "]:checked")
   ).map(function (el) {
-    console.log("este es el array checked hasta aca+++++ ", checked);
-    console.log("este es el hasta aca+++++ ", el.value);
-    againAproof(el.value);
-    return el.value; //retorna el nombre del evento
+    return el.value;
   });
 }
 
 
+function setVisibility(filteredProducts) {
 
-function setVisibility() {
-  allProducts.map(function (el) {
-    console.log("checked antes", checked);
+  filteredProducts.map(el => {
+    //console.log("checked antes", checked);
     var categories = checked.categories.length? _.intersection(Array.from(el.classList), checked.categories).length : true;
     var tags = checked.tags.length? _.intersection(Array.from(el.classList), checked.tags).length : true;
     var availability = checked.availability.length ? _.intersection(Array.from(el.classList), checked.availability).length : true;
-    console.log("categories", categories);
     if (categories && tags && availability) {
-      el.style.display = "block";
+     // el.style.display = "block";
+     el.style.display = "none";
       console.log("el event", el);
     } else {
-      el.style.display = "none";
+
     }
+    return el;
   });
 }
--- pagination.js
+++ pagination.js
@@ -1,56 +1,55 @@
-var result;
-function againAproof(value) {
-  console.log(
-    "este es el hasta aca*********************************************** ",
-    value
-  );
-  if (value) {
+  items = $(allProducts);
+  //console.log("items", items);
+  numItems = items.length;
+  //console.log("numItems", numItems);
+  perPage = 3;
+
+  items.slice(perPage).hide();
+
+  $("#pagination-container").pagination({
+    items: numItems,
+    itemsOnPage: perPage,
+    prevText: "Next",
+    nextText: "Previous>",
+    onPageClick: function (pageNumber) {
+      //console.log("entro a la funcion pagination", pageNumber);
+      var showFrom = perPage * (pageNumber - 1);
+      //console.log("showFrom", showFrom);
+      var showTo = showFrom + perPage;
+      console.log("showTo", showTo);
+      items.hide().slice(showFrom, showTo).show();
+    },
+  });
+{{/*function againAproof(value) {
+  var result;
+  console.log("+++++++++++++++++++++++++++", value)
+  if (value === 'undefined') {
+    result = allProducts;
+  } else {
     result = allProducts.filter((element) =>
-      element?.className.includes(value)
-    );
-    var items = $(result);
-    console.log("items", items);
-    var numItems = items.length;
-    console.log("numItems", numItems);
-    var perPage = 4;
-
-    items.slice(perPage).hide();
-
-    $("#pagination-container").pagination({
-      items: numItems,
-      itemsOnPage: perPage,
-      prevText: "Next",
-      nextText: "Previous>",
-      onPageClick: function (pageNumber) {
-        console.log("entro a la funcion pagination", pageNumber);
-        var showFrom = perPage * (pageNumber - 1);
-        console.log("showFrom", showFrom);
-        var showTo = showFrom + perPage;
-        console.log("showTo", showTo);
-        items.hide().slice(showFrom, showTo).show();
-      },
-    });
+      element?.className.includes(value));
   }
+  items = $(result);
+  console.log("items***********", items);
+  numItems = items.length;
+  console.log("numItems**********", numItems);
+  perPage = 4;
+
+  items.slice(perPage).hide();
+  console.log("luego del item slice");
+  $("#pagination-container").pagination({
+    items: numItems,
+    itemsOnPage: 4,
+    prevText: "Next",
+    nextText: "Previous>",
+    onPageClick: function (pageNumber) {
+      console.log("entro a la funcion pagination", pageNumber);
+      var showFrom = perPage * (pageNumber - 1);
+      console.log("showFrom", showFrom);
+      var showTo = showFrom + perPage;
+      console.log("showTo", showTo);
+      items.hide().slice(showFrom, showTo).show();
+    },
+  });
 }
-var items = $(allProducts);
-console.log("items", items);
-var numItems = items.length;
-console.log("numItems", numItems);
-var perPage = 4;
-
-items.slice(perPage).hide();
-
-$("#pagination-container").pagination({
-  items: numItems,
-  itemsOnPage: perPage,
-  prevText: "Next",
-  nextText: "Previous>",
-  onPageClick: function (pageNumber) {
-    console.log("entro a la funcion pagination", pageNumber);
-    var showFrom = perPage * (pageNumber - 1);
-    console.log("showFrom", showFrom);
-    var showTo = showFrom + perPage;
-    console.log("showTo", showTo);
-    items.hide().slice(showFrom, showTo).show();
-  },
-});
+*/}}
